856920b52b4062c4b60b1d843711c58050294652
mferraretto <matheus.ferraretto94@gmail.com>
2025-06-17 11:20:44 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,22 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import {
-  getAuth,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { firebaseConfig } from "./firebase-init.js";
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-document.getElementById("entrarBtn").addEventListener("click", () => {
-  const email = document.getElementById("email").value;
-  const senha = document.getElementById("senha").value;
-
-  signInWithEmailAndPassword(auth, email, senha)
-    .then(() => {
-      window.location.href = "painel.html";
-    })
-    .catch(error => {
-      alert("Erro no login: " + error.message);
-    });
-});
+function logout() {
+  alert("Logout efetuado!");
+  window.location.href = "login.html";
+}
