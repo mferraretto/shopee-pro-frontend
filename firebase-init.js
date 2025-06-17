811c9bcc4fed04d66c34a2de45e7687a7b1a12e8
mferraretto <matheus.ferraretto94@gmail.com>
2025-06-17 10:13:59 -0300

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,32 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  query,
-  orderBy
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
-// Configuração do seu Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBYsFfLeXm52UH254582MDxBphVJLDSPLI",
-  authDomain: "shopee-pro-d3f15.firebaseapp.com",
-  projectId: "shopee-pro-d3f15",
-  storageBucket: "shopee-pro-d3f15.firebasestorage.app",
-  messagingSenderId: "779191463935",
-  appId: "1:779191463935:web:239ed1459ed649b735c1ca"
+  apiKey: "AIzaSyDpeLLtMasPdbWcffrDZQ0a_G-CtBhkbVk",
+  authDomain: "shopee-pro-v2.firebaseapp.com",
+  projectId: "shopee-pro-v2",
+  storageBucket: "shopee-pro-v2.firebasestorage.app",
+  messagingSenderId: "627773073455",
+  appId: "1:627773073455:web:ba85fbd1f3a2661f267eda"
 };
 
-// Inicializa Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, signInWithEmailAndPassword, onAuthStateChanged, signOut, db, collection, addDoc, getDocs, query, orderBy };
+export const app = initializeApp(firebaseConfig);
