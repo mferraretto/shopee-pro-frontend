672105b42ac3f69985b63537bb6d7dd16241314f
mferraretto <matheus.ferraretto94@gmail.com>
2025-06-18 10:13:30 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,4 +1,11 @@
-export const firebaseConfig = {
+// firebase-init.js
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Configuração do Firebase
+const firebaseConfig = {
   apiKey: "AIzaSyDpeLLtMasPdbWcffrDZQ0a_G-CtBhkbVk",
   authDomain: "shopee-pro-v2.firebaseapp.com",
   projectId: "shopee-pro-v2",
@@ -6,3 +13,11 @@ export const firebaseConfig = {
   messagingSenderId: "627773073455",
   appId: "1:627773073455:web:ba85fbd1f3a2661f267eda"
 };
+
+// Inicialização do Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Exportações
+export { app, auth, db };
